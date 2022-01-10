e6bc3f9d7a385dc5f8e5cf0518ba5253390851ff
Hayeong8957 <judy.hayoung.shin@gmail.com>
2022-01-10 20:56:57 +0900

    Complete TodoList UI

---
+++ To-Do-List/mashup-todolist/README.md
@@ -0,0 +1,34 @@
+# To-do-List
+
+https://react.vlpt.us/mashup-todolist/01-create-components.html
+
+## 만들어야 할 컴포넌트
+
+### TodoTemplate
+
+- 투두리스트 레이아웃 설정
+- 페이지 중앙에 그림자가 적용된 흰색 박스
+
+### TodoHead
+
+- 오늘의 날짜와 요일을 보여줌
+- 앞으로 해야 할 일이 몇개 남았는지 보여줌
+
+### TodoList
+
+- 할 일에 대한 정보가 들어있는 todos 배열
+  -> 내장함수 `map`을 사용하여 여러개의 TodoItem컴포넌트를 렌더링
+
+### TodoItem
+
+- 각 할 일에 대한 정보 렌더링하는 컴포넌트
+- 좌측에 있는 원을 누르면 할 일의 완료 여부를 toggle
+- 할 일 완료시, 좌측 체크, 텍스트 색상 연해짐
+- 마우스 hover시 휴지통 아이콘, 항목 삭제
+
+### TodoCreate
+
+- 새로운 할 일을 등록할 수 있는 컴포넌트
+- TodoTemplate의 하단부에 초록색 원 버튼을 렌더링하고
+  이를 클릭하면 할 일을 입력할 수 있는 폼이 나타남.
+  버튼을 다시 누르면 폼이 사라짐
---
+++ To-Do-List/mashup-todolist/src/components/TodoHead.js
@@ -0,0 +1,42 @@
+// 오늘의 날짜, 요일, 남은 할 일 개수
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48px;
+  padding-left: 32px;
+  padding-right: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1px solid #e9ecef;
+  h1 {
+    // 날짜
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+  .day {
+    // 요일
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+  .tasks-left {
+    // 남은 할 일 개수
+    color: #20c997;
+    font-size: 18px;
+    margin-top: 40px;
+    font-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  return (
+    <TodoHeadBlock>
+      <h1>2022년 01월 10일</h1>
+      <div className="day">월요일</div>
+      <div className="tasks-left">할 일 3개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
